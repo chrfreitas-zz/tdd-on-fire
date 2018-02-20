4f2e312fd401023ea1fd471a292dec0f7887bc2f
Christian Freitas <chrfreitas@gmail.com>
2018-02-20 19:19:43 -0300

    Refactor

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+} from './search';
+
+module.exports = {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+};
