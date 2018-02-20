703cf58b85ca5cf47c8ae592e9f79fc66d2936c1
Christian Freitas <chrfreitas@gmail.com>
2018-02-20 15:04:48 -0300

    Fix lint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 export const search = (query, type) => {
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
-}
-export const searchAlbums = () => {}
-export const searchArtists = () => {}
-export const searchTracks = () => {}
-export const searchPlaylists = () => {}
+};
+
+export const searchAlbums = () => {};
+export const searchArtists = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
