dd088c8ee3e18cb9ee1d8019855419f535b0cef2
Christian Freitas <chrfreitas@gmail.com>
2018-02-20 18:56:45 -0300

    Fix lint errors

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
-export const search = (query, type) => {
-  return fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+export const search = (query, type) =>
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
     .then(data => data.json());
-};
 
-export const searchAlbums = (query) => {
-  return search(query, 'album');
-};
+export const searchAlbums = query => search(query, 'album');
 
-export const searchArtists = (query) => {
-  return search(query, 'artist');
-};
+export const searchArtists = query => search(query, 'artist');
 
-export const searchTracks = (query) => {
-  return search(query, 'track');
-};
+export const searchTracks = query => search(query, 'track');
 
-export const searchPlaylists = (query) => {
-  return search(query, 'playlist');
-};
+export const searchPlaylists = query => search(query, 'playlist');
